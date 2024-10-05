3d7ffd099dc26605a6b24f9b66c27a15598d639f
Pedro2708 <phernandezg7@miumg.edu.gt>
2024-10-05 10:54:40 -0600

    publish

---
+++ assets/QTr.48e78157.js
@@ -0,0 +1,1 @@
+import{H as t,p as s,x as a,S as p}from"./index.0eec7d61.js";var l=t({name:"QTr",props:{props:Object,noHover:Boolean},setup(r,{slots:e}){const o=s(()=>"q-tr"+(r.props===void 0||r.props.header===!0?"":" "+r.props.__trClass)+(r.noHover===!0?" q-tr--no-hover":""));return()=>a("tr",{class:o.value},p(e.default))}});export{l as Q};
---
+++ assets/use-quasar.1f64cd0a.js
@@ -0,0 +1,1 @@
+import{l as a,a7 as r}from"./index.0eec7d61.js";function u(){return a(r)}export{u};
