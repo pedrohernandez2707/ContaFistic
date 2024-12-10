0d5cfcf3c196ed7415740a759d2ff50b42d526ec
Pedro2708 <phernandezg7@miumg.edu.gt>
2024-12-10 09:33:25 -0600

    Publish Reportes, ultimos cambios Edwin

---
+++ assets/QTr.7d73f901.js
@@ -0,0 +1,1 @@
+import{I as t,q as s,y as a,U as n}from"./index.14025cc8.js";var l=t({name:"QTr",props:{props:Object,noHover:Boolean},setup(r,{slots:e}){const o=s(()=>"q-tr"+(r.props===void 0||r.props.header===!0?"":" "+r.props.__trClass)+(r.noHover===!0?" q-tr--no-hover":""));return()=>a("tr",{class:o.value},n(e.default))}});export{l as Q};
